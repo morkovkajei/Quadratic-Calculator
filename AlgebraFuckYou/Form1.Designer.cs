6c36bdc64fda80d28c8cb2bf2db6f4c68f8213c5
morkovkajei <123565584+morkovkajei@users.noreply.github.com>
2023-04-03 20:48:17 +0500

    Add files via upload

---
+++ AlgebraFuckYou/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace AlgebraFuckYou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(312, 154);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(161, 82);
+            this.button.TabIndex = 0;
+            this.button.Text = "calculate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X2:";
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(341, 56);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 22);
+            this.textB.TabIndex = 3;
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(538, 56);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 22);
+            this.textC.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(386, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(580, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(371, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(178, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "A";
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(134, 56);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 22);
+            this.textA.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 16);
+            this.label9.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(538, 153);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 16);
+            this.label10.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(541, 202);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 16);
+            this.label11.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ AlgebraFuckYou/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AlgebraFuckYou
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            //Переменные
+            double x1, x2, a, b, c, D;
+
+            //Получаем данные из TextBox
+            //Примечания : b = p , c = q
+            a = Convert.ToDouble(textA.Text);
+            b = Convert.ToDouble(textB.Text);
+            c = Convert.ToDouble(textC.Text);
+
+
+
+            //Ищем дискриминант
+            D = b*b + (-4 * a * c) ;
+
+            // На основании дискриминанта получаем ответ и выводим его
+            if (D < 0)
+            {
+                // Эта штука просто выводит по центру сколько корней
+                label5.Text = "Нет решения . Ни одного ";
+
+                // Это болеее подробное решение дискриминанта
+                label7.Text = "D = " + D;
+                label8.Text = "D = " + b * b + " + " + (-4 * a * c);
+                label9.Text = "D = " + b +"*"+ b + " + " + "(-4 *" +a +  "*" + c+")";
+
+            }
+            else if (D == 0) 
+            {
+                // Эта штука просто выводит по центру сколько корней
+                label5.Text = "Есть только одно решение";
+
+                x1 = (-(b) + Math.Sqrt(D)) / (2 * a);
+                x2 = (-(b) - Math.Sqrt(D)) / (2 * a);
+
+                // Это у нас более подробное решение по формуле и его вывод
+                //label10.Text = "( - (" + b + ")" + "√"+ "D" + "/"+ "(2 *"+ a;
+                //label11.Text = "( - (" + b + "-" + ")" + "√" + D + "/"+D + "/" + "(2 *" + a;
+
+                //Это тупой вывод в label
+                label1.Text = Convert.ToString("X1:" + x1);
+                label2.Text = Convert.ToString("X2:" + x2);
+
+                // Это болеее подробное решение дискриминанта
+                label7.Text = "D = " + D;
+                label8.Text = "D = " + b * b + " + " + (-4 * a * c);
+                label9.Text = "D = " + b + "*" + b + " + " + "(-4 *" + a + "*" + c + ")";
+
+            }
+            else
+            {
+                // Эта штука просто выводит по центру сколько корней
+                label5.Text = "Имеется 2 решения";
+
+                x1 = (-(b) + Math.Sqrt(D)) / (2 * a);
+                x2 = (-(b) - Math.Sqrt(D)) / (2 * a);
+
+                // Это у нас более подробное решение по формуле и его вывод
+                //label10.Text = "( - (" + b + ")" + "√" + "D" + "/" + "(2 *" + a;
+                //label11.Text = "( - (" + b + "-" + ")" + "√" + D + "/" + D + "/" + "(2 *" + a;
+
+                //Это тупой вывод в label
+                label1.Text = Convert.ToString("X1:" + x1);
+                label2.Text = Convert.ToString("X2:" + x2);
+
+                // Это болеее подробное решение дискриминанта
+                label7.Text = "D = " + D;
+                label8.Text = "D = " + b * b + " + " + (-4 * a * c);
+                label9.Text = "D = " + b + "*" + b + " + " + "(-4 *" + a + "*" + c + ")";
+            }
+            
+            
+            
+            
+
+            
+
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
